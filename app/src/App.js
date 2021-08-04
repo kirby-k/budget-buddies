46845c161fbd7922a0daad5a69a446f215a0927e
Ivy Chan <ivychan@northwesternmutual.com>
2021-08-04 09:25:04 -0700

    test routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import CreateBudget from './createBudget';
+import Dashboard from './dashboard';
+import Home from './home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/createBudget">Create Budget</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/createBudget">
+              <CreateBudget />
+            </Route>
+
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ app/src/routes/CreateBudget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CreateBudget = () => {
+  return (
+    <h1>hello world</h1>
+  );
+}
+
+export default CreateBudget
